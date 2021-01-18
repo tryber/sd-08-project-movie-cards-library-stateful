95b8fbca774c4507baf4ce47c8dcc1d05b28b252
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-18 15:26:34 -0300

    Requisito 5 concluído

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,16 +4,18 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre } = this.props;
+    const { searchText } = this.props;
     return (
       <div>
-        <p>{ searchText }</p>
-        <p>{ bookmarkedOnly }</p>
-        <p>{ selectedGenre }</p>
         <form data-testid="search-bar-form">
           <label htmlFor="text-input" data-testid="text-input-label">
             Inclui o texto
-            <input id="text-input" type="text" data-testid="text-input" />
+            <input
+              id="text-input"
+              type="text"
+              data-testid="text-input"
+              value={ searchText }
+            />
           </label>
         </form>
       </div>
@@ -23,8 +25,8 @@ class SearchBar extends Component {
 
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
-  bookmarkedOnly: PropTypes.bool.isRequired,
-  selectedGenre: PropTypes.string.isRequired,
+  // bookmarkedOnly: PropTypes.bool.isRequired,
+  // selectedGenre: PropTypes.string.isRequired,
 };
 
 export default SearchBar;
