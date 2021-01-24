1c3bac253834bae1597e28e12b280f5f9a4041c2
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-01-24 01:00:05 -0300

    r02 01

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      <section />
+      <section>
+        <form data-testid="search-bar-form"> </form>
+      </section>
     );
   }
 }
