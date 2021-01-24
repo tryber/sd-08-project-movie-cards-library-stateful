c9d97bca1b53b98c53bcf697574b5dee9d36e3f1
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-01-24 02:00:31 -0300

    r16 01

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,33 @@
-// implement MovieLibrary component here.
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      movies,
+    };
+  }
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie />
+        <MovieList movies={ movies } />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieLibrary;
