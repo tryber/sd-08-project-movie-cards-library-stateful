75839ab3cab55bad5a399d50eee00c637c6a2aab
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-01-24 01:35:51 -0300

    r08 01

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      title: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  inputTitle() {
+    const { title } = this.state;
+    return (
+      <label data-testid="title-input-label" htmlFor="title-input">
+        Título
+        <input
+          id="title-input"
+          type="text"
+          name="title"
+          value={ title }
+          onChange={ this.handleChange }
+          data-testid="title-input"
+        />
+      </label>
+    );
+  }
+
   render() {
     return (
-      <form data-testid="add-movie-form"> </form>
+      <form data-testid="add-movie-form">
+        { this.inputTitle() }
+      </form>
     );
   }
 }
