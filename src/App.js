b0de8d86d7916e17df96fae1374a15755bb668ce
vivianeflowt <vivianeflowt@gmail.com>
2021-01-27 01:40:57 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/Header';
+import shell from 'shelljs';
 
 function App() {
+
+  const do = () =>{
+    //
+    console.log("kkkkkkkkkkkkkkkkkkkkkkk")
+  }
+
   return (
-    <div className="App">
-      <Header />
+    <div>
+      <p>lol</p>
+      {do()}
     </div>
   );
 }
